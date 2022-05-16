ad120011a3697dee9a2dde738fea8cc4cc2b8abc
YLPsam <105673478+YLPsam@users.noreply.github.com>
2022-05-16 15:39:01 -0400

    Add files via upload

---
+++ validar.php
@@ -0,0 +1,26 @@
+<?php
+$usuario=$_POST['usuario'];
+$contraseña=$_POST['contraseña'];
+session_start();
+$_SESSION['usuario']=$usuario;
+
+$conexion=mysqli_connect("localhost,root,,login")
+
+$consulta="SELECT*FROM usuarios where usuario='$usuario' and " contraseña='$contraseña'";
+$resultado=mysqli_query($conexion ,$consulta);
+
+$filas=mysqli_num_rows($resultado);
+
+if($filas){
+    header("location:home.php");
+
+}else{
+    ?>
+    <?php
+    include("index.php");
+    ?>
+    <h1 class="bad">ERROR DE AUTENTIFICACIÓN</h1>
+    <?php
+}
+mysli_free_result($resultado);
+mysqli_close($conexion);
